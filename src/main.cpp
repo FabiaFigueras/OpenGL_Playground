5b33bfb7312e7207b5314ebeb6169d6bde09fcf2
Fabià Figueras <fabiafigueras@gmail.com>
2019-03-17 23:10:16 +0100

    Added GLFW and GLEW dependencies
    Added the OpenGL necessary dependencies and updated the MakeFile so it
    builds correctly the project

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
-#include <iostream>
+#include <GL/glew.h>
+#define GLFW_DLL
+#include <GLFW/glfw3.h>
+
+#include <stdio.h>
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    if (!glfwInit()) {
+        fprintf(stderr, "ERROR: Could not start GLFW3\n");
+    }
+
+    fprintf(stdout, "PROGRAM IS WORKING");
+
+    glfwTerminate();
+
     return 0;
 }
